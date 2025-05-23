b7f0163358696641233c4ca88cb0ff8a119b3ed0
roastmemaster <roastmemaster@gmail.com>
2025-05-23 15:24:27 -0400

    Update webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -2,7 +2,10 @@
 import { buffer } from 'micro';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe('', {
+  apiVersion: '2024-04-10',
+});
+
 
 export const config = {
   api: {
